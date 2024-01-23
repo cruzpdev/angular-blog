e3f1023b024e54de69992b6197aa61e342ecf3d5
cruzp <pvmariani88@gmail.com>
2024-01-23 16:25:46 -0300

    add id/route to all pages (console.log verify)

--- angular-blog/src/app/pages/content/content.component.ts
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-content',
@@ -7,11 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
   photoCover:string = "https://avatars.githubusercontent.com/u/114313554?v=4"
-  contentTitle:string = ""
-  contentDescription:string = ""
-  constructor() { }
+  contentTitle:string = "Meu Artigo"
+  contentDescription:string = "Olá mundo!"
+  constructor(
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      console.log(value.get("id"))
+    )
   }
 
 }
